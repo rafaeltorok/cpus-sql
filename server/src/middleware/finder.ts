ed34da9ac8c74e3b16802815bd66f70ca90336e1
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 18:36:47 -0300

    add: cpu object finder middleware to simplify the express routes

---
+++ server/src/middleware/finder.ts
@@ -0,0 +1,15 @@
+// Models
+import Cpu from "../models/cpu.js";
+
+// TypeScript types
+import type { Request, Response, NextFunction } from "express";
+
+// Handles finding a CPU based on its id
+export default async function cpuFinder(req: Request, res: Response, next: NextFunction) {
+  req.cpu = await Cpu.findByPk(req.params.id);
+  
+  if (!req.cpu) {
+    return res.status(404).end();
+  }
+  next();
+}
