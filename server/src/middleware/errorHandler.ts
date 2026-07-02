f8f912123374f08b74c14e4f618e63d76709adf2
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 16:45:10 -0300

    update: implement error handler middleware to be used inside of the Express routes

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import type { Request, Response, NextFunction } from "express";
+
+export default function errorHandler(err: unknown, _req: Request, res: Response, next: NextFunction) {
+  if (process.env.NODE_ENV !== "test") {
+    console.error(err); // Log on the server for debugging
+  }
+
+  if (err instanceof Error) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(400).json({ error: "CPU must be unique" });
+    }
+
+    if (err.name === "SequelizeValidationError") {
+      if (err.errors.length > 1) {
+        return res
+          .status(400)
+          .json({ error: err.errors.map((error) => error.message) });
+      } else {
+        return res.status(400).json({ error: err.errors[0].message });
+      }
+    }
+  } else {
+    return res.status(500).json({ error: "internal server error" });
+  }
+}
