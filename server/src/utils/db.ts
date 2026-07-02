12a7e44d2435ef97a4e798bff18b98be9919978b
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 18:35:40 -0300

    refactor: implement sequelize for the server database communication

---
+++ server/src/utils/db.ts
@@ -0,0 +1,35 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+import { DATABASE_URL } from "./config.js";
+
+// TypeScript types
+
+let sequelize: Sequelize;
+
+if (process.env.DATABASE_SSL === "true") {
+  // SSL is required for remote databases
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else if (process.env.DATABASE_SSL === "false") {
+  // Disable SSL connections for the Docker orchestrations
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: false,
+    },
+  });
+} else {
+  throw new Error(
+    "The env variable 'DATABASE_SSL' must be set to either true or false",
+  );
+}
+
+export default sequelize;
