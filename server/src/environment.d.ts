e71be9585d9eedaf23357f8b2428cc7cbdf0f7b1
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 18:31:22 -0300

    update: the environment declaration script to remove the optional definitions

--- server/src/environment.d.ts
+++ server/src/environment.d.ts
@@ -3,8 +3,8 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: "development" | "production" | "test";
       PORT?: string;
-      DATABASE_URL?: string;
-      DATABASE_SSL?: "true" | "false";
+      DATABASE_URL: string;
+      DATABASE_SSL: "true" | "false";
     }
   }
 }
