3655c674465879c957a3b492d88da73ab5cf3069
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 18:37:11 -0300

    add: id validation middleware for the express routes

---
+++ server/src/middleware/validators/validateId.ts
@@ -0,0 +1,21 @@
+import { param, validationResult } from "express-validator";
+
+// TypeScript types
+import type { Request, Response, NextFunction } from "express";
+
+const validateId = [
+  param("id").notEmpty().withMessage("Missing ID value"),
+  param("id").isInt({ min: 0 }).withMessage("Invalid ID format"),
+
+  // Handles the error response
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ error: errorMessages });
+    }
+    next();
+  },
+];
+
+export default validateId;
