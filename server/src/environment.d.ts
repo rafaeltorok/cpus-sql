cb7666585ce3fb6309362777df91d91a611ccfa5
rafaeltorok <rafaeltorok@gmail.com>
2026-07-02 16:42:18 -0300

    add: env variables declaration types script

---
+++ server/src/environment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production" | "test";
+      PORT?: string;
+      DATABASE_URL?: string;
+      DATABASE_SSL?: "true" | "false";
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
