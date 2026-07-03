aa5a65f26c3fdddd331b6400cf3cafad60a4df6e
Rafael Torok <rafaeltorok@gmail.com>
2026-07-03 12:19:12 -0300

    update: remove unused async function declaration

--- server/src/controllers/health.ts
+++ server/src/controllers/health.ts
@@ -7,7 +7,7 @@ const healthRouter = express.Router();
 
 healthRouter.get(
   "/",
-  async (_req: Request, res: Response, next: NextFunction) => {
+  (_req: Request, res: Response, _next: NextFunction) => {
     return res.status(200).send("Server is online");
   },
 );
