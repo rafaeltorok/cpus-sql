a5bf756c5fd80378844d3b9206cafd7669d5c131
rafaeltorok <rafaeltorok@gmail.com>
2026-07-03 16:25:54 -0300

    fix: the conditional block to work with any number of error messages

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,27 +1,30 @@
+import { ValidationError, UniqueConstraintError } from "sequelize";
+
+// ENV variables setup
 import dotenv from "dotenv";
 dotenv.config();
 
+// TypeScript types
 import type { Request, Response } from "express";
 
+// Middleware
 export default function errorHandler(
   err: unknown,
   _req: Request,
   res: Response,
 ) {
   if (err instanceof Error) {
-    if (err.name === "SequelizeUniqueConstraintError") {
+    if (err instanceof UniqueConstraintError) {
       return res.status(400).json({ error: "CPU must be unique" });
     }
 
-    if (err.name === "SequelizeValidationError") {
-      if (err.errors.length > 1) {
-        return res
-          .status(400)
-          .json({ error: err.errors.map((error) => error.message) });
-      } else {
-        return res.status(400).json({ error: err.errors[0].message });
-      }
+    if (err instanceof ValidationError) {
+      return res
+        .status(400)
+        .json({ error: err.errors.map((error) => error.message) });
     }
+
+    return res.status(500).json({ error: String(err) });
   } else {
     return res.status(500).json({ error: "internal server error" });
   }
