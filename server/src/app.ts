710e6f54e04d11df32b52c7daa30c37b08b99b5a
Rafael Torok <rafaeltorok@gmail.com>
2026-07-04 08:52:40 -0300

    update: add support for a test route and serving the static build of the frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,7 @@ import errorHandler from "./middleware/errorHandler.js";
 // Routes
 import cpusRouter from "./controllers/cpus.js";
 import healthRouter from "./controllers/health.js";
+import testsRouter from "./controllers/tests.js";
 
 const app = express();
 
@@ -17,9 +18,22 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
+
+// Serve the static build for the frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('dist'));
+}
+
+// Routes
 app.use("/api/cpus", cpusRouter);
 app.use("/api/health", healthRouter);
 
+// Route to reset the test database data
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/reset", testsRouter);
+}
+
+// Error handler middleware
 app.use(errorHandler);
 
 export default app;
