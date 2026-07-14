d0bdcf6de82ad88ad9289ef4314c136fedd5a9f6
Rafael Torok <rafaeltorok@gmail.com>
2026-07-14 11:55:14 -0300

    update: re-organized the tests into valid and invalid objects blocks
    - add new tests for invalid data
    
    - include a helper function to prevent code duplication when checking the amount of objects inside of the database

--- server/tests/integration/cpus/post_route.test.ts
+++ server/tests/integration/cpus/post_route.test.ts
@@ -19,6 +19,16 @@ import type { CpuType } from "../../../types/types.ts";
 
 const api = supertest(app);
 
+// Helper functions
+async function getAmount(): number {
+	const response = await api
+		.get("/api/cpus")
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
+		
+	return response.body.length;
+}
+
 before(async () => {
   // Connect to the PostgreSQL database
   await setupDb();
@@ -35,48 +45,235 @@ describe("POST /api/cpus", () => {
     await Cpu.truncate({ restartIdentity: true });
   });
   
-  test("a new processor can be created", async () => {
-	// Get a sample processor to be added
-	const cpu = processors[0];
-	  
-	// Get the initial list of processors
-	const initialProcessors = await api
-	  .get("/api/cpus")
-	  .expect(200)
-	  .expect("Content-Type", /application\/json/);
-	  
-    // Add a new processor
-    const postResponse = await api
-      .post("/api/cpus")
-      .send(cpu)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
-       
-    // Get the current list of processors
-	const currentProcessors = await api
-	  .get("/api/cpus")
-	  .expect(200)
-	  .expect("Content-Type", /application\/json/);
+  describe("Valid processor data", () => {
+		test("a new processor can be created", async () => {
+			// Get a sample processor to be added
+			const cpu = processors[0];
+				
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(cpu)
+				.expect(201)
+				.expect("Content-Type", /application\/json/);
+				 
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
 
-    // Confirm the amount of processors has been increased
-    assert.strictEqual(currentProcessors.body.length, initialProcessors.body.length + 1);
-  });
-  
-  test("the processor specifications should be correctly returned within te response", async () => {
-	// Get a sample processor to be added
-	const cpu = processors[0];
-	  
-    // Add a new processor
-    const postResponse = await api
-      .post("/api/cpus")
-      .send(cpu)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
-      
-    // Remove the id field from the response
-    const { id, ...otherFields } = postResponse.body;
+			// Confirm the amount of processors has been increased
+			assert.strictEqual(currentProcessors, initialProcessors + 1);
+		});
+		
+		test("returns all specifications within the response", async () => {
+			// Get a sample processor to be added
+			const cpu = processors[0];
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(cpu)
+				.expect(201)
+				.expect("Content-Type", /application\/json/);
+				
+			// Remove the id field from the response
+			const { id, ...otherFields } = postResponse.body;
 
-    // Confirm the returned data is correct
-    assert.deepStrictEqual(processors[0], otherFields);
-  });
+			// Confirm the returned data is correct
+			assert.deepStrictEqual(processors[0], otherFields);
+		});
+		
+		test("multiple processors can be added", async () => {
+			// Add all available test data
+			for (const processor of processors) {
+				const postResponse = await api
+					.post("/api/cpus")
+					.send(processor)
+					.expect(201)
+					.expect("Content-Type", /application\/json/);
+			}
+				 
+			// Get the current amount of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the total number of processors is present
+			assert.deepStrictEqual(processors.length, currentProcessors);
+		});
+		
+		test("a float value for the cache should be valid", async () => {
+			// Get a sample processor to be added
+			const originalProcessor = processors.find(
+				(processor: CpuType) => processor.model === "Core i9-10900K"
+			);
+			
+			// Add the processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(originalProcessor)
+				.expect(201)
+				.expect("Content-Type", /application\/json/);
+				
+			// Extract only the cache field from the response
+			const { cache, ...otherFields } = postResponse.body;
+
+			// Confirm the returned data is correct
+			assert.deepStrictEqual(originalProcessor.cache, cache);
+		});
+	});
+	
+	describe("Invalid processor data", () => {
+		test("returns a proper status code and error messages with an empty request body", async () => {
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there are error messages within the response
+			assert.ok(postResponse.body.error.length > 0);
+		});
+		
+		test("returns error messages with an invalid processor name", async () => {
+			const invalidData = {
+				...processors[0],
+				manufacturer: "",
+				model: "",
+			};
+			
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(invalidData)
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there are error messages within the response
+			assert.ok(postResponse.body.error.includes("Manufacturer is required"));
+			assert.ok(postResponse.body.error.includes("Model is required"));
+		});
+		
+		test("returns an error message with a zero value", async () => {
+			const invalidData = {
+				...processors[0],
+				cores: 0,
+			};
+			
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(invalidData)
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there is an error message within the response
+			assert.ok(postResponse.body.error.includes("Invalid amount of cores"));
+		});
+		
+		test("returns an error message with a negative value", async () => {
+			const invalidData = {
+				...processors[0],
+				cores: -1,
+			};
+			
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(invalidData)
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there is an error message within the response
+			assert.ok(postResponse.body.error.includes("Invalid amount of cores"));
+		});
+		
+		test("returns an error message with a non-numeric value", async () => {
+			const invalidData = {
+				...processors[0],
+				cores: "8",
+			};
+			
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(invalidData)
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there is an error message within the response
+			assert.ok(postResponse.body.error.includes("Invalid core amount"));
+		});
+		
+		test("returns an error message with a null value", async () => {
+			const invalidData = {
+				...processors[0],
+				cores: null,
+			};
+			
+			// Get the initial list of processors
+			const initialProcessors = await getAmount();
+			
+			// Add a new processor
+			const postResponse = await api
+				.post("/api/cpus")
+				.send(invalidData)
+				.expect(400)
+				.expect("Content-Type", /application\/json/);
+
+			// Get the current list of processors
+			const currentProcessors = await getAmount();
+
+			// Confirm the amount of processors has not changed
+			assert.strictEqual(currentProcessors, initialProcessors);
+			
+			// Assert there is an error message within the response
+			assert.ok(postResponse.body.error.includes("Invalid core amount"));
+		});
+	});
 });
